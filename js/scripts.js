e4e3ecbaee1db102a42e82c06d584b7523d0cba3
danielcaixetadesousa <danielcaixetadesousa@gmail.com>
2022-06-14 23:36:57 -0300

    primeiro commit

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+var nome = "Xavier";
+
+if(nome =="Pedro") {
+    console.log("O nome dele não é Pedro");
+} else if(nome == "Daniel") {
+    console.log("O nome é Daniel");
+} else if(nome == "Xavier") {
+        console.log("O nome é Xavier");
+    } else {
+    console.log("Ele possui outro nome!"); //nesse caso ele executo o else
+}
+
+/* porém se o nome fosse o mesmo ele ignora o else
+
+var nome = "Pedro";
+
+if(nome =="Pedro") {
+    console.log("O nome dele não é Pedrp");
+} else {
+    console.log("Ele possui outro nome!"); 
+}
+
+*/
+
+var idade = 19;
+
+if(idade > 20) {
+    console.log("Ele pode entrar na festa!");
+} else if(idade >= 18) {
+    console.log("Ele só pode entrar com autorização")
+}
+
+if(nome == "Daniel") {
+    console.log("teste");
+} else {
+    console.log("testando");
+}
